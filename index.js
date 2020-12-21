4601b7568ed2bbfd0f8cfe4b341804d68820a500
shotius <sh.archemashvili@gmail.com>
2020-12-21 21:33:44 +0400

    started mongodb

--- index.js
+++ index.js
@@ -1,89 +1,55 @@
+require('dotenv').config();
 const { response } = require("express");
-const cors = require('cors');
 const express = require("express");
-const morgan = require('morgan');
 const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+const Note = require('./models/notes') // db model
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
-]
-
+// -- middlewares
 app.use(morgan('tiny'))
 app.use(express.json());
 app.use(cors());
 app.use(express.static('build'))
 
-app.get('/', (req, res) => {
-res.send("<h1>I Wish My Knopka Were Here <3 </h1>")
-});
-
 app.get("/api/notes", (req, res) => {
-    res.json(notes);
+        
+    Note.find({}).then(result => {
+      res.json(result);
+    })
+
 });
 
 app.get('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const note = notes.find(note =>  note.id === id )
-
-    if (note){
-        res.json(note);
-    } else {
-        res.status(400).end();
-    }
+   Note.findById(req.params.id).then(foundNote => {
+     res.json(foundNote)
+   })
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id);
-    notes = notes.filter(note => note.id !== id);
-
-    res.status(204).end();
+    Note.findByIdAndDelete(req.params.id).then(note => {
+      res.json(note)
+    })
 })
-
-const generateId = () => {
-  const maxId = notes.length > 1
-  ? Math.max(...notes.map(note => note.id))
-  : 0  
-
-  return maxId + 1
-}
 
 app.post('/api/notes', (req, res) => {
   const body = req.body
 
   if (!body.content){
-    return res.status(400).json({
-      error: "missing content"
-    });
+    return res.status(400).json({error: "missing content"});
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId()
-  }
+  })
 
-  notes = notes.concat(note)
-    
-  res.json(note)
-})
+  note.save().then(savedNote => {
+    console.log("note is saved");
+    res.json(savedNote)
+  })
+});
 
 
 const PORT = process.env.PORT || 3001
