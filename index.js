ad804bf56ae174eb3a576b9c6b9ccba30b8d12a3
shotius <sh.archemashvili@gmail.com>
2021-01-19 20:20:34 +0400

    part3 in before deploying database in backend

--- index.js
+++ index.js
@@ -6,34 +6,23 @@ const cors = require('cors');
 const morgan = require('morgan');
 const Note = require('./models/notes') // db model
 
+
+
 // -- middlewares
+app.use(express.static('build'))
 app.use(morgan('tiny'))
 app.use(express.json());
 app.use(cors());
-app.use(express.static('build'))
 
 app.get("/api/notes", (req, res) => {
-        
     Note.find({}).then(result => {
       res.json(result);
     })
-
 });
 
-app.get('/api/notes/:id', (req, res) => {
-   Note.findById(req.params.id).then(foundNote => {
-     res.json(foundNote)
-   })
-});
-
-app.delete('/api/notes/:id', (req, res) => {
-    Note.findByIdAndDelete(req.params.id).then(note => {
-      res.json(note)
-    })
-})
-
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', (req, res, next) => {
   const body = req.body
+
 
   if (!body.content){
     return res.status(400).json({error: "missing content"});
@@ -45,12 +34,69 @@ app.post('/api/notes', (req, res) => {
     date: new Date(),
   })
 
-  note.save().then(savedNote => {
-    console.log("note is saved");
-    res.json(savedNote)
-  })
+  // save note to db 
+  note
+    .save()
+    .then(savedNote => savedNote.toJson())
+    .then(savedAndFormatedNote => {
+      console.log("note is saved in db");
+      res.json(savedAndFormatedNote);
+    })
+    .catch(error => next(error))
 });
 
+app.get('/api/notes/:id', (req, res, next) => {
+   Note.findById(req.params.id)
+      .then(note => {
+          if (note){
+            res.json(note)
+          } else {
+            res.status(404).end();
+          }
+      })
+      .catch(error => next(error))
+});
+
+app.delete('/api/notes/:id', (req, res, next) => {
+    Note.findByIdAndRemove(req.params.id)
+      .then(note => {
+        res.status(204).end()
+      })
+      .catch(err => next(err))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+
+    const note = {
+      content: body.content,
+      important: body.important
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+        .then(updatedNote => {
+          response.json(updatedNote)
+        })
+        .catch(err => next(err))
+})
+
+// error handler middleware 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  // check if id of the note is valid
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === "ValidationError"){
+    // check if al validations are setisfite during save document to db
+    return response.status(400).json({ error: error.message });
+  } 
+
+  next(error)
+}
+
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
--- models/notes.js
+++ models/notes.js
@@ -14,8 +14,15 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
 
 // schema and model for notes
 const noteSchema = mongoose.Schema({
-    content: String,
-    data: Date,
+    content: {
+      type: String,
+      minLength: 5,
+      required: true
+    },
+    date: {
+      type: Date,
+      required: true
+    },
     important: Boolean
   })
 
